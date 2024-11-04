b84f1caec56bb260d9a4ad3f22cedda69ac95399
IvanMerino515 <merdix515@gmail.com>
2024-11-04 18:29:47 +0100

    updt

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+
+// Aquí se guardan las variables del css
+//const rootStyles = document.documentElement.style;
+
+//rootStyles.setProperty(nombreVariable, nuevoValor);
+
+
+
+// const asciiBackground = document.getElementById('ascii-background');
+// const characters = ['@', '#', '$', '%', '&', '+', '=', '-', ':', ';', 'I', '/', '\\', '|', '~', 'N', 'A', 'X']; // Carácteres a rotar
+// const columns = Math.floor(window.innerWidth / 10); // Ajustado para celdas de 10x10 px
+// const rows = Math.floor(window.innerHeight / 10);
+// const totalCells = columns * rows;
+
+// // Crea celdas con caracteres aleatorios
+// for (let i = 0; i < totalCells; i++) {
+//   const span = document.createElement('span');
+//   span.textContent = characters[Math.floor(Math.random() * characters.length)];
+//   asciiBackground.appendChild(span);
+// }
+
+// // Función para actualizar caracteres aleatoriamente cada 500 ms
+// function updateCharacters() {
+//   const spans = asciiBackground.querySelectorAll('span');
+//   spans.forEach(span => {
+//     span.textContent = characters[Math.floor(Math.random() * characters.length)];
+//   });
+// }
+
+// // Actualiza los caracteres cada 500 milisegundos
+// setInterval(updateCharacters, 10);
